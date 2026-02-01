d1306c563d442bd8b784d11786753957b53355c8
abeonweb <bmospecific@yahoo.com>
2026-02-01 20:04:14 +0100

    create custom error classes

---
+++ domain/errors.ts
@@ -0,0 +1,23 @@
+export class IssueAlreadyResolvedError extends Error {
+  constructor(issueId: string) {
+    super(`Issue ${issueId} is already resolved`);
+  }
+}
+
+export class IssueNotFoundError extends Error {
+  constructor(issueId: string) {
+    super(`Issue ${issueId} not found`);
+  }
+}
+
+export class InvalidResolutionError extends Error {
+  constructor() {
+    super("Resolution requires a root cause");
+  }
+}
+
+export class IssueResolutionMismatchError extends Error {
+  constructor() {
+    super("The resolution does not match the issue");
+  }
+}
