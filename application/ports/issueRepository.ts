e9d3ada26f0eda630cdf98fd9c063cd9482f7bd5
abeonweb <bmospecific@yahoo.com>
2026-02-03 07:42:10 +0100

    update isue repository to include search

--- application/ports/issueRepository.ts
+++ application/ports/issueRepository.ts
@@ -1,8 +1,12 @@
 import { Issue } from "@/domain/issue/issue";
 
+export type IssueSearchOptions = {
+  project?: string;
+  sort?: "relevance" | "recent"
+};
 export interface IssueRepository {
   getById(id: string): Promise<Issue | null>;
   save(issue: Issue): Promise<void>;
   update(issue: Issue): Promise<void>;
-  search(query: string): Promise<Issue[]>;
+  search(query: string, options: IssueSearchOptions): Promise<Issue[]>;
 }
