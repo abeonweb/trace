ae99a6b81c489fb2f073c9719ff9ff2d3143573f
abeonweb <bmospecific@yahoo.com>
2026-02-16 05:55:25 +0000

    move prisma singleton to composition folder

--- composition/container.ts
+++ composition/container.ts
@@ -1,8 +1,6 @@
 import { PrismaIssueRepository } from "@/lib/db/prismaIssueRepository";
 import { PrismaResolutionRepository } from "@/lib/db/prismaResolutionRepository";
-import { PrismaClient } from "@prisma/client/extension";
-
-export const prisma = new PrismaClient();
+import prisma from "./prisma";
 
 export const issueRepo = new PrismaIssueRepository(prisma);
 export const resolutionRepo = new PrismaResolutionRepository(prisma);
